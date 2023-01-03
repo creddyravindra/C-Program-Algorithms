1de1cc4627baf13e42938556b171df79f7915d55
creddyravindra <114750275+creddyravindra@users.noreply.github.com>
2023-01-03 15:10:44 +0530

    Create merge_sort.c

---
+++ All Programs/merge_sort.c
@@ -0,0 +1,49 @@
+/*C program for Merge sort*/
+#include<stdio.h>
+int b[5000];
+void merge(int a[],int low,int mid,int high)
+{
+    int i,j,k;
+    i=low;
+    j=mid+1;
+    k=low;
+    while(i<=mid && j<=high)
+    {
+        if(a[i]<=a[j])
+            b[k++]=a[i++];
+        else
+            b[k++]=a[j++];
+    }
+    while(i<=mid)
+    	b[k++]=a[i++];
+    while(j<=high)
+      	b[k++]=a[j++];
+    for(k=low;k<=high;k++)
+      	a[k]=b[k];
+}
+void mergesort(int a[],int low,int high)
+{
+    int mid;
+    if(low>=high)
+     	return;
+    mid = (low+high)/2;
+    mergesort(a,low,mid);
+    mergesort(a,mid+1,high);
+    merge(a,low,mid,high);
+}
+int main()
+{
+    int n,a[5000],k;
+    double ts;
+    printf("\n enter how many numbers:");
+    scanf("%d",&n);
+    printf("the random numbers are :\n");
+    for(k=1;k<=n;k++)
+        scanf("%d",&a[k]);
+    printf("%d\t",a[k]);
+    mergesort(a,1,n);
+    printf("\n Sorted Numbers  are:\n");
+    for(k=1;k<=n;k++)
+    	printf("%d\t",a[k]);
+    return 0;
+}
