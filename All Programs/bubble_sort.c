07a359a6b5e2d1c951d136df3ba71106287cdb9e
creddyravindra <114750275+creddyravindra@users.noreply.github.com>
2023-01-03 15:07:12 +0530

    Create bubble_sort.c

---
+++ All Programs/bubble_sort.c
@@ -0,0 +1,34 @@
+/*C Program for Bubble sort*/
+#include<stdio.h>
+void swap(int *xp,int *yp){
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+void bubblesort(int a[],int n){
+	int i,j;
+	for(i=0;i<=n-2;i++){
+		for(j=0;j<=n-2-i;j++)
+			if(a[j+1]<a[j])
+				swap(&a[j],&a[j+1]);
+	}
+}
+void printarray(int a[],int size){
+	int i;
+	for(i=0;i<size;i++){
+		printf("%d ",a[i]);
+	}
+}
+int main(){
+	int a[10],i,n;
+	printf("enter your array size number :");
+	scanf("%d",&n);
+	printf("enter your elements :");
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	bubblesort(a,n);
+	printf("sorted array : ");
+	printarray(a,n);
+	return 0;
+}
